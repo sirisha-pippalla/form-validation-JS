5432696b03402b1530c3c48025115b0b5e7f6e24
sirishapippalla <sirisha.pippalla7@gmail.com>
2023-04-08 00:43:09 +0530

    first commit

--- formValidation/result.js
+++
@@ -1,8 +0,0 @@
-const paragraph = document.getElementById("paragraph")
-const params = new URLSearchParams(window.location.search);
-
-params.forEach((key, value) => {
-    paragraph.append(`${key} = ${value}`);
-    paragraph.append(document.createElement('br'))
-    
-});
